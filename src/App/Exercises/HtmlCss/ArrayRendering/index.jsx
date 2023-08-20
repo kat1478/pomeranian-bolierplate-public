3cc5c98b7e0f3a3beafcfa67c22dea8e7f52de76
Katarzyna Kadyszewska <kadyszewskak@gmail.com>
2023-08-20 19:00:52 +0200

    dodanie tabeli z zawartością, dodanie styli

--- src/App/Exercises/HtmlCss/ArrayRendering/index.jsx
+++ src/App/Exercises/HtmlCss/ArrayRendering/index.jsx
@@ -2,7 +2,12 @@ import { StarIcon } from '../../../Components/Icons/StarIcon';
 import './styles.css';
 
 export function Exercise() {
-  const cars = ['Saab', 'Volvo', 'BMW', 'Skoda', 'Mercedes'];
+  const cars = [
+    { id: 1, make: 'Saab', model: '92B', year: 1953, owner: 'Jon' },
+    { id: 2, make: 'Volvo', model: 'C40', year: 2021, owner: 'Jenny' },
+    { id: 3, make: 'Audi', model: 'A3', year: 2019, owner: 'Michael' },
+    { id: 4, make: 'Tesla', model: 'Model S', year: 2022, owner: 'Sarah' },
+  ];
 
   // Saab => <li>Saab</li>
 
@@ -16,11 +21,30 @@ export function Exercise() {
 
   return (
     <div className="array-rendering">
-      <ul>
+      <ol>
         {cars.map(function (car) {
-          return <li key={car}>{car}</li>;
+          return (
+            <li key={car.id}>
+              {car.make} {car.model} produced in {car.year}
+            </li>
+          );
         })}
-      </ul>
+      </ol>
+      <table>
+        <th>Make</th>
+        <th>Model</th>
+        <th>Year</th>
+
+        {cars.map(function (car) {
+          return (
+            <tr key={car.id}>
+              <td>{car.make}</td>
+              <td>{car.model}</td>
+              <td>{car.year}</td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 }
